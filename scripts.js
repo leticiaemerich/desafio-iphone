c17a7fb3588fc5d93a74cd8846e72493591c10e3
leticiaemerich <leticiacantilhoemerich@gmail.com>
2023-05-02 23:06:36 -0300

    desafio-iphone

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+function trocaImg (imagem) {
+    let imgIphones = document.querySelector(".iphones")
+    imgIphones.src = imagem
+}
